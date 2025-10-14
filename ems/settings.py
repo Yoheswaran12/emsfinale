a53acdb4fbe6121f12fdc9307a9a80db975f0e5a
Yoheswaran12 <rgyoheswaran6543@gmail.com>
2025-10-14 17:12:44 +0530

    Add Render domain to ALLOWED_HOSTS

--- ems/settings.py
+++ ems/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-in-production'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emsfinale.onrender.com', 'localhost', '127.0.0.1']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
